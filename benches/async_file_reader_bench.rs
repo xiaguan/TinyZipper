bef8a13e3938069d6e105bb7e57ffc1a13fdb2b4
susun <751080330@qq.com>
2023-03-01 02:56:50 +0800

    add async file reader 's bench

---
+++ benches/async_file_reader_bench.rs
@@ -0,0 +1,88 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion, async_executor};
+
+use tiny_zipper::zipper::file_buf_reader::*;
+use tokio::io::AsyncWriteExt;
+use tokio::sync::watch;
+
+async fn test_func(){
+            // generate a 1GB file for test
+        // read it with 1KB buffer 
+        // calculate the time cost and throughput
+
+        println!("start to generate the test file");
+        const TEST_FILE_SIZE : usize = 10 * 1024 * 1024;
+
+        let std_start = std::time::Instant::now();
+        let mut file = tokio::fs::File::create("test_file").await.unwrap();
+        let buf = vec![19u8;1024];
+        let mut write_size = 0;
+        while write_size < TEST_FILE_SIZE {
+            file.write_buf(&mut buf.as_slice()).await.unwrap();
+            write_size += buf.len();
+        }
+        file.flush().await.unwrap();
+        let std_end = std::time::Instant::now();
+        println!("std write file cost: {:?}",std_end.duration_since(std_start));
+
+        let (tx,rx) = watch::channel(ReadBuf{order:0,buf:vec![],read_size:0});
+        let mut file_reader = AsyncFileReader::new("test_file",1024,tx).await.unwrap();
+
+        let read_num = 4;
+
+        // start the timer
+        let start = std::time::Instant::now();
+
+        // spwan the async read task
+        let sender_handle = tokio::spawn(async move {
+            file_reader.read().await;
+            for _ in 0..read_num {
+                file_reader.send_eof();
+            }
+        });
+
+        // create read_num consumers to raed the data from the queue
+        let mut reciver_handles = Vec::new();
+        for _ in 0..read_num {
+            let mut curr_rx = rx.clone();
+            let reciver_handle =    tokio::spawn(async move{
+                // let mut read_size = 0;
+                // let mut order = 0 ;
+                while curr_rx.changed().await.is_ok() {
+                    //assert_eq!(order,curr_rx.borrow().order);
+                    let read_buf = curr_rx.borrow();
+                    if read_buf.read_size == 0 {
+                        break;
+                    }
+                    // read_size += read_buf.read_size;
+                    // order += 1;
+                }
+                //println!("read_size: {},expected_file_size: {} ",read_size,TEST_FILE_SIZE);
+                //assert_eq!(read_size,TEST_FILE_SIZE);
+            });
+            reciver_handles.push(reciver_handle);
+        }
+        sender_handle.await.unwrap();
+        for reciver_handle in reciver_handles {
+            reciver_handle.await.unwrap();
+        }
+
+        // stop the timer
+        let end = std::time::Instant::now();
+        let time_cost = end.duration_since(start);
+        let throughput = TEST_FILE_SIZE as f64 / time_cost.as_secs_f64();
+        println!("time_cost: {:?},throughput: {} MB/s",time_cost,throughput / 1024.0 / 1024.0);
+}
+
+fn un_async_func()
+{
+    //println!("????");
+    let rt = tokio::runtime::Runtime::new().unwrap();
+    rt.block_on(test_func());
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("fib 20", |b| b.iter(|| un_async_func()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
--- src/zipper/file_buf_reader.rs
+++ src/zipper/file_buf_reader.rs
@@ -3,9 +3,9 @@ use tokio::{sync::watch, io::AsyncReadExt};
 
 #[derive(Debug)]
 pub struct ReadBuf{
-    order : usize,
-    buf : Vec<u8>,
-    read_size : usize,
+    pub order : usize,
+    pub buf : Vec<u8>,
+    pub read_size : usize,
 }
 // use tokio lib to async read the fixed size of data from the file
 // then put the data into a single producer multiple consumer queue
@@ -67,6 +67,8 @@ impl AsyncFileReader {
 // add test for the async file reader
 #[cfg(test)]
 mod tests{
+    use std::cell::RefCell;
+
     use crate::zipper::file_buf_reader::*;
     use tokio::{sync::watch, io::AsyncWriteExt};
 
@@ -118,5 +120,75 @@ mod tests{
         reciver_handle.await.unwrap();
         
     }
-}
 
+    #[tokio::test]
+    async fn bench_test(){
+        // generate a 1GB file for test
+        // read it with 1KB buffer 
+        // calculate the time cost and throughput
+
+
+        const TEST_FILE_SIZE : usize = 1024 * 1024 * 1024;
+
+        let std_start = std::time::Instant::now();
+        let mut file = tokio::fs::File::create("test_file").await.unwrap();
+        let buf = vec![19u8;1024];
+        let mut write_size = 0;
+        while write_size < TEST_FILE_SIZE {
+            file.write_buf(&mut buf.as_slice()).await.unwrap();
+            write_size += buf.len();
+        }
+        file.flush().await.unwrap();
+        let std_end = std::time::Instant::now();
+        println!("std write file cost: {:?}",std_end.duration_since(std_start));
+
+        let (tx,rx) = watch::channel(ReadBuf{order:0,buf:vec![],read_size:0});
+        let mut file_reader = AsyncFileReader::new("test_file",1024,tx).await.unwrap();
+
+        let read_num = 4;
+
+        // start the timer
+        let start = std::time::Instant::now();
+
+        // spwan the async read task
+        let sender_handle = tokio::spawn(async move {
+            file_reader.read().await;
+            for _ in 0..read_num {
+                file_reader.send_eof();
+            }
+        });
+
+        // create read_num consumers to raed the data from the queue
+        let mut reciver_handles = Vec::new();
+        for _ in 0..read_num {
+            let mut curr_rx = rx.clone();
+            let reciver_handle =    tokio::spawn(async move{
+                let mut read_size = 0;
+                let mut order = 0 ;
+                while curr_rx.changed().await.is_ok() {
+                    assert_eq!(order,curr_rx.borrow().order);
+                    let read_buf = curr_rx.borrow();
+                    if read_buf.read_size == 0 {
+                        break;
+                    }
+                    read_size += read_buf.read_size;
+                    order += 1;
+                }
+                //println!("read_size: {},expected_file_size: {} ",read_size,TEST_FILE_SIZE);
+                assert_eq!(read_size,TEST_FILE_SIZE);
+            });
+            reciver_handles.push(reciver_handle);
+        }
+        sender_handle.await.unwrap();
+        for reciver_handle in reciver_handles {
+            reciver_handle.await.unwrap();
+        }
+
+        // stop the timer
+        let end = std::time::Instant::now();
+        let time_cost = end.duration_since(start);
+        let throughput = TEST_FILE_SIZE as f64 / time_cost.as_secs_f64();
+        println!("time_cost: {:?},throughput: {} MB/s",time_cost,throughput / 1024.0 / 1024.0);
+
+    }
+}
